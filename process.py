ce1791f22a5e6393a163d326968e5a9777f9d3de
rousez <zdrouse@gmail.com>
2017-10-10 00:11:55 -0400

    Adjustments to structure and implementation of counting both pairs and triplets of items in the retail data file.

--- process.py
+++ process.py
@@ -2,27 +2,51 @@ from transaction import Transaction
 from collections import Counter
 import time
 
-start = time.clock()
+retail_file = "retail.dat"
+history_triplet_log = []
+history_pair_log = []
 
-with open("retail.dat", "r") as f:
-	history_log = []
-	count = 0
-	for line in f:
-		trans = Transaction(count, map(int, line.split()))
-		parse = trans.get_items()
-		# we only care about the co-occurences
-		for item in range(len(parse) - 1):
-			history_log.append((parse[item], parse[item + 1]))
-			
-			#print(parse[item], parse[item + 1])
-		count += 1
-		#print(" ")
-	#print sorted(history_log)
-	counter = Counter(sorted(history_log))
-	print counter.most_common(1)
+def get_highest_count(array_log):
+	# use Counter to count all of the occurences of items in the log.
+	highest_counter = Counter(array_log).most_common(1)
+	# print counter.most_common(1)
+	for combination, occurence in highest_counter:
+		return combination, occurence
 
-f.close()
 
-end = time.clock()
+def get_pair_log(items):
+	# we only care about the co-occurences
+	for item in range(len(items) - 1):
+		history_pair_log.append((items[item], items[item + 1]))
+		# print(items[item], items[item + 1])
+	return None
 
-print("\nFinished in %s seconds." % (end - start))
+
+def get_triplet_log(items):
+	# we only care about the triple-occurences
+	for item in range(len(items) - 2):
+		history_triplet_log.append((items[item], items[item + 1], items[item + 2]))
+	return None
+
+def main():
+	with open(retail_file, "r") as retail_data:
+		start = time.clock()
+		count = 0
+		for line in retail_data:
+			trans = Transaction(count, map(int, line.split()))
+			parsed_items = trans.get_items()
+			get_pair_log(parsed_items)
+			get_triplet_log(parsed_items)
+			count += 1
+	print("File: %s contains %d lines.\n" % (retail_file, count))
+	pairs, pair_count = get_highest_count(history_pair_log)
+	triplets, triplet_count = get_highest_count(history_triplet_log)
+	print("The highest co-occurence item pairs in the file are: %s  with count %d" % (str(pairs), pair_count))
+	print("The highest co-occurence item triplets in the file are: %s  with count %d" % (str(triplets), triplet_count))
+	retail_data.close()
+	end = time.clock()
+	print("\nFinished in %s seconds." % (end - start))
+
+if __name__ == "__main__":
+	main()
+	
